c43295e74c1b2a0c2925f95c74f3325805a8c2b3
GerazimCode <gerazimcode@gmail.com>
2021-09-20 13:45:48 -0400

    add remove button to the program

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 let form = document.getElementById("registrar");
 let input = document.querySelector("input");
+let ul = document.getElementById("invitedList");
 
 // add a submit eventlistener to the form
 form.addEventListener('submit', (event)=> {
@@ -9,7 +10,6 @@ form.addEventListener('submit', (event)=> {
     // adding text to the list items
     let text = input.value;
     input.value = '';
-    let ul = document.getElementById("invitedList");
     let li = document.createElement("li");
     li.textContent = text;
 
@@ -20,7 +20,27 @@ form.addEventListener('submit', (event)=> {
     checkbox.type = "checkbox";
     label.appendChild(checkbox);
     li.appendChild(label);
+
+    // add a romove button to each list item
+    let button = document.createElement("button");
+    button.textContent = "Remove";
+    li.appendChild(button);
+
     // append the li to the UL
     ul.appendChild(li);
+
+})
+
+// add eventListener to the checkbox itself
+ul.addEventListener("change", (event) => {
+    let checkbox = event.target;
+    let checked = checkbox.checked;
+    let listItem = checkbox.parentNode.parentNode; //this will access the list element as a grand parent of checkbox.
+
+    if (checked){
+        listItem.className = "responded";
+    }else{
+        listItem.className = "";
+    }
 
 })
